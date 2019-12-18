c84fc9e4e56bbf0ba8aff209c3fe60cdf39e066b
Lúcio Leandro <lucio.santana@solutis.com.br>
2019-12-18 17:25:09 -0300

    FutureBuild

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,32 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gringotts/database/app_database.dart';
+import 'package:gringotts/models/contact.dart';
 import 'package:gringotts/screens/contact_form.dart';
 
 class ContactsList extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contact List'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Lúcio',
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
-              ),
-              subtitle: Text(
-                'Leandro',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          )
-        ],
+      body: FutureBuilder(
+        future: findAll(),
+        builder: (context, snapshot) {
+          final List<Contact> contacts = snapshot.data;
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final Contact contact = contacts[index];
+              return _ContactItem(contact);
+            },
+            itemCount: contacts != null ? contacts.length : 0,
+          );
+        },
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context)
@@ -34,12 +33,34 @@ class ContactsList extends StatelessWidget {
                 MaterialPageRoute(
                   builder: (context) => ContactForm(),
                 ),
-              )
-              .then(
-                (newContact) => debugPrint(newContact.toString()),
               );
+
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  const _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
